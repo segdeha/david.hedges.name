e2d02c38799d9aa457be183cddd38b9142c17025
Andrew Hedges <andrew@hedges.name>
2020-12-23 09:30:29 -0800

    Figured out how to pre-process page HTML, stubbed a file for where that can live across pages

---
+++ pages/graphql/utils.js
@@ -0,0 +1,4 @@
+
+// input: take an array of pages and a slug
+// output: filtered array of pages for the slug with transformations applied
+// first transformation: converting \n to <br>
--- pages/poems/poems.11tydata.js
+++ pages/poems/poems.11tydata.js
@@ -2,7 +2,15 @@ const { getPages } = require('../graphql/data');
 
 module.exports = async () => {
   const allPages = await getPages();
-  const pages = allPages.filter(page => page.category && page.category.slug === 'poems');
+  const pages = allPages
+    .filter(page => page.category && page.category.slug === 'poems')
+    .map(page => {
+        return {
+            ...page,
+            html: page.body.html.replace("\n", '<br>'),
+        };
+    })
+  ;
   return {
     pages,
   };
